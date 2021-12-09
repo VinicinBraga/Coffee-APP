df8bdc7adf2ef37e13a40cfb53ceaa0bb1a6f9f3
VinicinBraga <vinicius@bragamatta.com.br>
2021-12-09 19:19:05 -0300

    Refatoring Cards

--- front-end/src/components/Cards/index.js
+++ front-end/src/components/Cards/index.js
@@ -5,10 +5,8 @@ export default function Cards(props) {
     <div className="card-container">
       <h2>Produto: {props.coffeeName}</h2>
       <h3>Description: {props.coffeeDescription}</h3>
-      <h3>Price: {props.coffeePrice}</h3>
-
+      <h3>Price: $ {props.coffeePrice},00</h3>
       <div>
-        <input type="text" />
         <button className="update-btn">Update Card</button>
       </div>
       <button className="delet-btn">Delete Card</button>
